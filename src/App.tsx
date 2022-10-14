3a4bc3d2ae715659e032392bd27297f38c9a216d
luckstar77 <luckstar77y@gmail.com>
2022-10-14 21:12:27 +0800

    styled

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,11 @@ import Typography from '@mui/material/Typography';
 import axios from 'axios';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import Box from '@mui/material/Box';
 
 const STOCK_API_URL = 'http://localhost:3000/stock';
 
@@ -44,75 +49,85 @@ function App() {
 
     return (
         <div>
-            <TextField 
-                id="outlined-basic" 
-                label="請輸入股票代號或名稱" 
-                variant="outlined" 
-                value={search}
-                onChange={handleSearchChange}
-            />
-            <Button variant="contained" onClick={() => {
-                axios.get(STOCK_API_URL,{
-                    params: {
-                        search
-                    }
-                }).then(({data}) => {
-                    const {allAvgCashYields,
-                        allAvgRetroactiveYields,
-                        amountOfDividend,
-                        amountOfSuccess,
-                        dividendYearEnd,
-                        dividendYearStart,
-                        successRate,
-                        id,
-                        name} = data;
-                    setAllAvgCashYields(allAvgCashYields.toFixed(2));
-                    setAllAvgRetroactiveYields(allAvgRetroactiveYields.toFixed(2));
-                    setAmountOfDividend(amountOfDividend);
-                    setAmountOfSuccess(amountOfSuccess);
-                    setDividendYearEnd(dividendYearEnd);
-                    setDividendYearStart(dividendYearStart);
-                    setSuccessRate(successRate.toFixed(2));
-                    setId(id);
-                    setName(name);
-                }).catch((error) => {
-                    setOpen(true);
-                    setErrorMessage(error);
-                });
-            }}>確定</Button>
-            <Card sx={{ minWidth: 275 }}>
-                <CardContent>
-                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                    代號: {id}
-                    </Typography>
-                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                    名稱: {name}
-                    </Typography>
-                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                    填權息成功率: {successRate}%
-                    </Typography>
-                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                    除權息次數: {amountOfDividend}
-                    </Typography>
-                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                    填權息次數: {amountOfSuccess}
-                    </Typography>
-                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                    統計年分: {dividendYearStart} - {dividendYearEnd}
-                    </Typography>
-                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                    歷年平均現金殖利率: {allAvgCashYields}%
-                    </Typography>
-                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                    歷年平均還原殖利率: {allAvgRetroactiveYields}%
-                    </Typography>
-                </CardContent>
-            </Card>
+            
             <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
                 <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
                     {errorMessage.toString()}
                 </Alert>
             </Snackbar>
+            <Grid container 
+                direction="row"
+                justifyContent="center"
+                alignItems="center">
+                <Box sx={{
+                    height: '1000',
+                    minHeight: '1000',
+                    marginTop:25,
+                }}><TextField 
+                        id="outlined-basic" 
+                        label="請輸入股票代號或名稱" 
+                        variant="outlined" 
+                        value={search}
+                        onChange={handleSearchChange}
+                    />
+                    <Button variant="contained" onClick={() => {
+                        axios.get(STOCK_API_URL,{
+                            params: {
+                                search
+                            }
+                        }).then(({data}) => {
+                            const {allAvgCashYields,
+                                allAvgRetroactiveYields,
+                                amountOfDividend,
+                                amountOfSuccess,
+                                dividendYearEnd,
+                                dividendYearStart,
+                                successRate,
+                                id,
+                                name} = data;
+                            setAllAvgCashYields(allAvgCashYields.toFixed(2));
+                            setAllAvgRetroactiveYields(allAvgRetroactiveYields.toFixed(2));
+                            setAmountOfDividend(amountOfDividend);
+                            setAmountOfSuccess(amountOfSuccess);
+                            setDividendYearEnd(dividendYearEnd);
+                            setDividendYearStart(dividendYearStart);
+                            setSuccessRate(successRate.toFixed(2));
+                            setId(id);
+                            setName(name);
+                        }).catch((error) => {
+                            setOpen(true);
+                            setErrorMessage(error);
+                        });
+                    }}>確定</Button>
+                    <Card sx={{ minWidth: 275 }}>
+                        <CardContent>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    代號: {id}
+                            </Typography>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    名稱: {name}
+                            </Typography>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    填權息成功率: {successRate}%
+                            </Typography>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    除權息次數: {amountOfDividend}
+                            </Typography>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    填權息次數: {amountOfSuccess}
+                            </Typography>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    統計年分: {dividendYearStart} - {dividendYearEnd}
+                            </Typography>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    歷年平均現金殖利率: {allAvgCashYields}%
+                            </Typography>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    歷年平均還原殖利率: {allAvgRetroactiveYields}%
+                            </Typography>
+                        </CardContent>
+                    </Card></Box>
+            </Grid>
         </div>
     );
 }
